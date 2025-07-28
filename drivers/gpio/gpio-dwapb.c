9797c6e8da99a37082093c5bfdd92a386b700e1f
Jisheng Zhang <jszhang@kernel.org>
2025-07-28 17:10:06 +0800

    gpio-dwapb: add 'no-suspend-resume' property
    copid from linux_4_9
    
    Change-Id: Ia945c02cbc7fc2ecbec7006399bc14699589ece9
    Signed-off-by: Jisheng Zhang <jszhang@kernel.org>
    Reviewed-on: https://gerrit-sha.synaptics.com/gerrit/269918
    Reviewed-by: Jisheng Zhang <Jisheng.Zhang@synaptics.com>

--- drivers/gpio/gpio-dwapb.c
+++ drivers/gpio/gpio-dwapb.c
@@ -52,8 +52,9 @@
 #define GPIO_SWPORT_DDR_STRIDE	0x0c /* register stride 3*32 bits */
 
 #define GPIO_REG_OFFSET_V1	0
-#define GPIO_REG_OFFSET_V2	1
+#define GPIO_REG_OFFSET_V2	BIT(0)
 #define GPIO_REG_OFFSET_MASK	BIT(0)
+#define GPIO_NO_SUSPEND_RESUME	BIT(1)
 
 #define GPIO_INTMASK_V2		0x44
 #define GPIO_INTTYPE_LEVEL_V2	0x34
@@ -736,6 +737,9 @@ static int dwapb_gpio_probe(struct platform_device *pdev)
 
 	gpio->flags = (uintptr_t)device_get_match_data(dev);
 
+	if (device_property_read_bool(dev, "no-suspend-resume"))
+		gpio->flags |= GPIO_NO_SUSPEND_RESUME;
+
 	for (i = 0; i < gpio->nr_ports; i++) {
 		err = dwapb_gpio_add_port(gpio, &pdata->properties[i], i);
 		if (err)
@@ -754,6 +758,9 @@ static int dwapb_gpio_suspend(struct device *dev)
 	struct gpio_chip *gc	= &gpio->ports[0].gc;
 	unsigned long flags;
 	int i;
+
+	if (gpio->flags & GPIO_NO_SUSPEND_RESUME)
+		return 0;
 
 	raw_spin_lock_irqsave(&gc->bgpio_lock, flags);
 	for (i = 0; i < gpio->nr_ports; i++) {
@@ -795,6 +802,9 @@ static int dwapb_gpio_resume(struct device *dev)
 	struct gpio_chip *gc	= &gpio->ports[0].gc;
 	unsigned long flags;
 	int i, err;
+
+	if (gpio->flags & GPIO_NO_SUSPEND_RESUME)
+		return 0;
 
 	err = clk_bulk_prepare_enable(DWAPB_NR_CLOCKS, gpio->clks);
 	if (err) {
