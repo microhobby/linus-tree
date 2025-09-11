a78e13132f087acb7bbe650f48fe84b4e7476cb0
Jisheng Zhang <jszhang@kernel.org>
2025-09-11 18:15:42 +0800

    i3c: dw: Add apb reset signal support
    Change-Id: I82ae15a1d5b0050c29548cf43eb29e5a6dd78751
    Signed-off-by: Jisheng Zhang <jszhang@kernel.org>
    Reviewed-on: https://gerrit-sha.synaptics.com/gerrit/274433
    Reviewed-by: Jisheng Zhang <Jisheng.Zhang@synaptics.com>
    Reviewed-by: Kaicheng Xie <Kaicheng.Xie@synaptics.com>

--- drivers/i3c/master/dw-i3c-master.c
+++ drivers/i3c/master/dw-i3c-master.c
@@ -1570,7 +1570,13 @@ int dw_i3c_common_probe(struct dw_i3c_master *master,
 	if (IS_ERR(master->core_rst))
 		return PTR_ERR(master->core_rst);
 
+	master->apb_rst = devm_reset_control_get_optional_exclusive(&pdev->dev,
+								    "apb_rst");
+	if (IS_ERR(master->apb_rst))
+		return PTR_ERR(master->apb_rst);
+
 	reset_control_deassert(master->core_rst);
+	reset_control_deassert(master->apb_rst);
 
 	spin_lock_init(&master->xferqueue.lock);
 	INIT_LIST_HEAD(&master->xferqueue.list);
@@ -1617,6 +1623,7 @@ err_disable_pm:
 
 err_assert_rst:
 	reset_control_assert(master->core_rst);
+	reset_control_assert(master->apb_rst);
 
 	return ret;
 }
@@ -1716,6 +1723,7 @@ static int __maybe_unused dw_i3c_master_runtime_suspend(struct device *dev)
 	dw_i3c_master_disable(master);
 
 	reset_control_assert(master->core_rst);
+	reset_control_assert(master->apb_rst);
 	dw_i3c_master_disable_clks(master);
 	pinctrl_pm_select_sleep_state(dev);
 	return 0;
@@ -1728,6 +1736,7 @@ static int __maybe_unused dw_i3c_master_runtime_resume(struct device *dev)
 	pinctrl_pm_select_default_state(dev);
 	dw_i3c_master_enable_clks(master);
 	reset_control_deassert(master->core_rst);
+	reset_control_deassert(master->apb_rst);
 
 	dw_i3c_master_set_intr_regs(master);
 	dw_i3c_master_restore_timing_regs(master);
